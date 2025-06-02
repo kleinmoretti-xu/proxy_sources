44e769a43dff7ce502b55b3ee95959976902aa1e
50660 <506609505@qq.com>
2025-06-02 19:00:51 +0800

    youku adblock

--- script/loon/youku/youku_2.0.js
+++ script/loon/youku/youku_2.0.js
@@ -2,6 +2,8 @@ const url = $request.url;
 if (url.includes("mtop.youku.play.ups.appinfo.get")) {
     let obj = JSON.parse($response.body);
     delete obj.data.data.ad;
+    delete obj.data.data.ykad;
+    delete obj.data.data.stream;
     $done({body: JSON.stringify(obj)});
 } else {
     $done({});
