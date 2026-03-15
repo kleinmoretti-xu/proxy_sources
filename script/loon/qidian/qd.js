e6447d1872be1531f80911dc5b29d5566dba57f2
kleinmoretti <506609505@qq.com>
2026-03-15 09:53:46 +0800

    qd循环时间更新

--- script/loon/qidian/qd.js
+++ script/loon/qidian/qd.js
@@ -2,37 +2,41 @@ var task1 = $persistentStore.read("TASK_1");
 var session1 = $persistentStore.read("session_1");
 var task2 = $persistentStore.read("TASK_2");
 var session2 = $persistentStore.read("session_2");
-var waitTime = 8;
 
 var sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
-var doTask = async (session) => {
-    var options = JSON.parse(session);
-    return $httpClient.post(options, (errormsg, response, data) => {
-        var obj = JSON.parse(data);
-        if (obj.Result === 0) {
-            console.log("🎉成功!");
-        } else {
-            console.log("🔴失败!");
-            console.log(data);
-        }
+var doTask = (session) => {
+    return new Promise((resolve) => {
+        var options = JSON.parse(session);
+        $httpClient.post(options, (err, resp, data) => {
+            if (err) {
+                console.log("🔴网络错误");
+            } else {
+                var obj = JSON.parse(data);
+                console.log(obj.Result === 0 ? "🎉成功!" : "🔴失败: " + data);
+            }
+            resolve();
+        });
     });
 }
 
-var loop = async (step, session) => {
+var loop = async (step, session, delay) => {
     for (var i = 0; i < step; i++) {
         console.log(`🟡任务执行次数: ${i + 1}次`);
         await doTask(session);
         if (i < (step - 1)) {
-            await sleep(waitTime * 1000);
+            await sleep(delay * 1000);
         }
     }
 }
 
 if (task1 && session1) {
-    (async () => await loop(10, session1))()
+    (async () => {
+        await loop(9, session1, 6);
+        $done();
+    })();
 }
 
 if (task2 && session2) {
-    (async () => await loop(3, session2))()
+    (async () => await loop(3, session2, 8))()
 }
